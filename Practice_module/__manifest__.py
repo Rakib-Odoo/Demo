6dbb4722f35fab1a6cfe105782ce7bc4a5af42b2
Rakib48-code <shahidulhuda0@gmail.com>
2024-12-22 22:36:56 +0600

    models create

---
+++ Practice_module/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name' : 'Hospital Management',
+    'version' : '1.1.1',
+    'category' : 'Hospital',
+    'summary': 'Hospital Management System',
+    'author' : 'Rakib',
+    'sequence':1,
+    'depends' : [],
+    'data' : [],
+    'application': True,
+    'installable': True,
+}
---
+++ Practice_module/models/patient.py
@@ -0,0 +1,4 @@
+from odoo import api, fields, models
+
+class HospitalPatient(models.Model):
+    _name = 'hospital.patient'
