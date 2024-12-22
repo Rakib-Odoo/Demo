2643df01665736a1e933fe6684da77de0d482b38
Rakib48-code <shahidulhuda0@gmail.com>
2024-12-22 22:47:59 +0600

    create patient model

--- Practice_module/models/patient.py
+++ Practice_module/models/patient.py
@@ -4,7 +4,7 @@ class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital Patient'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name', required=True)
     ref = fields.Char(string='Reference')
     age = fields.Integer(string='Age', compute='age_compute')
     gender = fields.Selection([('male','Male'),
