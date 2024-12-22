91c12a288228f5f5b81bbf1d3ee940e13d9701e7
Rakib48-code <shahidulhuda0@gmail.com>
2024-12-22 22:42:21 +0600

    patient model create

--- Practice_module/models/patient.py
+++ Practice_module/models/patient.py
@@ -2,3 +2,13 @@ from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
+    _description = 'Hospital Patient'
+
+    name = fields.Char(string='Name')
+    ref = fields.Char(string='Reference')
+    age = fields.Integer(string='Age', compute='age_compute')
+    gender = fields.Selection([('male','Male'),
+                               ('female','Female'),
+                               ], string='Gender')
+    date_of_birth = fields.Date(string='Date of Birth')
+    image = fields.Image(string='image')
