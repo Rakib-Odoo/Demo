89b0614aa3c5def28f4e7c28e076d961feababef
Rakib48-code <shahidulhuda0@gmail.com>
2024-12-23 22:57:35 +0600

    appointment model done

--- Practice_module/__manifest__.py
+++ Practice_module/__manifest__.py
@@ -10,6 +10,7 @@
         'securtiy/ir.model.access.csv',
         'views/menu.xml',
         'views/patient.xml',
+        'views/appointment.xml',
     ],
     'application': True,
     'installable': True,
